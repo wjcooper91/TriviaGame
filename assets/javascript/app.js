1f700815060928d5eac9cd57a05117783146e90d
wjcooper91 <wjcooper91@gmail.com>
2018-06-19 23:14:38 -0400

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,44 +6,12 @@
 
     
 $(document).ready(function() {
-   
-
-    var btfquestions = [{
-        question1: "Which song is playing as Marty enters the Cafe 80s in Part II?",
-        choices1: ["Billie Jean", "Beat It", "Rock the Casbah", "You Better You Bet"],
-        correctAnswer1: 1
-    }, {
-        question2: "What phrase does the Doc repeat throughout the trilogy?",
-        choices2: ["Dax", "Darren", "Dave", "Dixon"],
-        correctAnswer2: 3
-    }, {
-        question3: "What phrase does the Doc repeat throughout the trilogy?",
-        choices3: ["My God!", "Sweet Baby Jesus!", "Great Scott!", "By George!"],
-        correctAnswer3: 3
-    }, {
-        question4: "What does Doc pack into his suitcase in case they don't have it in the future?",
-        choices4: ["Coffe", "Cotton Underwear", "Radio", "A Gun"],
-        correctAnswer4: 1
-    }, {
-        question5: "What date was George McFly murdered in Back to the Future Part II?",
-        choices5: ["March 15, 1973", "March 21, 1971", "March 19, 1972", "March 17, 1974"],
-        correctAnswer5: 0
-    }];
 
     var q1 = "C";
     var q2 = "B";
     var correctCounter = 0;
-
-    function correct(qNum, choice) {
-    
-        if (qNum == "q1" && choice == "C") {
-            correctCounter += 1;
-        }
-
-        if (qNum == "q2" && choice == "B") {
-            correctCounter += 1;
-        }
-    }
+    var incorrectCounter = 0;
+    var unansweredCounter = 0;
 
     $(".quiz").hide();
     $("#results").hide();
@@ -54,7 +22,7 @@ $(document).ready(function() {
         $(".quiz").show();
 
         (function() {
-            var counter = 19999;
+            var counter = 8;
 
             setInterval(function() {
                 counter--;
@@ -62,48 +30,55 @@ $(document).ready(function() {
                     span = document.getElementById("timer");
                     span.innerHTML = counter;
                 }
-                // Display 'counter' wherever you want to display it.
                 if (counter === 0) {
                     clearInterval(counter);
+                    var checked = $(":checked")
                     $("#timer").remove();
                     $(".quiz").remove();
                     $(".done").remove();
                     $("#results").show();
-                }
-
-                else {
-                    $(".done").click(function() {
-                        clearInterval(timer);
-                        var checked = $(":checked")
-                        $("#timer").remove();
-                        $(".quiz").remove();
-                        $(".done").remove();
-                        console.log(checked);
-                        for (var i = 0; i < checked.length; i++) {
-                            if ($(checked[i]).data("correct")) {
-                                correctCounter++;
-                            }
+                    for (var i = 0; i < checked.length; i++) {
+                        if ($(checked[i]).data("correct")) {
+                            correctCounter++;
                         }
-                        // for (var i = 0; i < 0; i++){
-
-                        //     $(data-correct == true )
-
-                        //     // if correct 
-                        //     correct();
-                        // }
-
-                        //for loop
-                        //grab list of children id from quiz, grab selected radio button
-                        //loop through array, each time call correct();
-                        $("#results").show();
+                        else {
+                            incorrectCounter ++;
+                        }
+                    }
                     $(".correct").append("Correct: " + correctCounter);
-                    });
+                    $(".incorrect").append("Incorrect: " + incorrectCounter);
+                
                 }
-
             }, 1000);
+
+            $("#done").click(function() {
+                clearInterval(counter);
+                var checked = $(":checked")
+                var empty = $('input:radio[name=question]:checked').val();
+                $("#timer").remove();
+                $(".quiz").remove();
+                $(".done").remove();
+                $("#results").show();
+                for (var i = 0; i < checked.length; i++) {
+                    if ($(checked[i]).data("correct")) {
+                        correctCounter ++;
+                    }
+                    else {
+                        incorrectCounter ++;
+                    }
+                }
+                $(".correct").append("Correct: " + correctCounter);
+                $(".incorrect").append("Incorrect: " + incorrectCounter);
+            });
 
         })();
     });
     
 
 });
+radio = $('input:radio[name=priority]:checked').val();
+
+// if radio hv checked
+if(radio !== ""){
+ // do something
+}
